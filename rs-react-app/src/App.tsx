191bcdd3dac69aac4df664f886f55eb6a4bf0b8a
soola-sys <agdarbekovsultan@gmail.com>
2025-12-10 23:31:05 +0500

    feat: create Courses component

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,14 +1,17 @@
-import "./App.css";
-import "./css/header.css";
+import './App.css';
+import './css/header.css';
+import './css/courses.css';
 
-import Header from "./components/Header.tsx";
+import Header from './components/Header';
+import Courses from './components/Courses';
 
 export default function App() {
   return (
     <>
-       <div className="wrapper">
-           <Header />
-       </div>
+      <div className="wrapper">
+        <Header />
+        <Courses />
+      </div>
     </>
   );
 }
--- rs-react-app/src/components/Header.tsx
+++ rs-react-app/src/components/Header.tsx
@@ -1,21 +1,21 @@
-import logo from "../assets/Logo_img.svg";
+import logo from '../assets/Logo_img.svg';
 
 const Header = () => {
-    return <>
-        <header className="header">
-            <div className="logo">
-                <a href="#">
-                    <img src={logo} alt="Courses Logo" />
-                </a>
-            </div>
-            <div className="header-inner">
-                <div>
-                    <h2>Harry Potter</h2>
-                </div>
-                <button className="header-btn">Logout</button>
-            </div>
-        </header>
-    </>;
+  return (
+    <>
+      <header className="header">
+        <div className="logo">
+          <a href="#">
+            <img src={logo} alt="Courses Logo" />
+          </a>
+        </div>
+        <div className="header-inner">
+          <p className="header-text">Harry Potter</p>
+          <button className="header-btn btn-primary">Logout</button>
+        </div>
+      </header>
+    </>
+  );
 };
 
-export default Header
+export default Header;
